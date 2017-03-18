74ac52ef9dc7d4946dd8409f0696774e289096e4
akh-rman <akh-rman@rambler.ru>
2017-03-18 20:16:13 +0300

    Внес изменения в для добавления адаптивности страницы.

--- client.php
+++ client.php
@@ -20,20 +20,28 @@ print("Не правильный формат даты");
 }
 }
 ?>
+<!doctype html>
 <html>
     <head>
         <title>Soap клиент</title>
         <link rel="stylesheet" href="CSS/style.css">
+        <meta name = "viewport" content="width = device-width, initial-scale = 1">
         <meta charset="utf-8">
     </head>
     <body>
-        <div>
-            <form method="POST" action="">
-                <input type="text" name="startDate" placeholder="Введите начальную дату:">
-                <input type="text" name="endDate" placeholder="Введите конечную дату:">
-                <input type="submit" value="Отправить">
-            </form>  
-        </div>
+        <form method="POST" action="" class="main-content">
+           <div class="row">
+               <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5">
+                   <input type="text" name="startDate" placeholder="Введите начальную дату:">   
+               </div>
+               <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5">
+                   <input type="text" name="endDate" placeholder="Введите конечную дату:">
+               </div>
+               <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+                   <input type="submit" value="Отправить">
+               </div>
+           </div>        
+        </form>  
         <?php
         if(isset($result)){
          echo $result;
