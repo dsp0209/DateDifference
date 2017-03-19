89427bba48320e8bbbd60458a00b685625de5967
akh-rman <akh-rman@rambler.ru>
2017-03-19 19:25:38 +0300

    Добавил header и footer.

--- client.php
+++ client.php
@@ -25,23 +25,37 @@ print("Не правильный формат даты");
     <head>
         <title>Soap клиент</title>
         <link rel="stylesheet" href="CSS/style.css">
-        <meta name = "viewport" content="width = device-width, initial-scale = 1">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
         <meta charset="utf-8">
+        
     </head>
     <body>
-        <form method="POST" action="" class="main-content">
-           <div class="row">
-               <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5">
-                   <input type="text" name="startDate" placeholder="Введите начальную дату:">   
-               </div>
-               <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5">
-                   <input type="text" name="endDate" placeholder="Введите конечную дату:">
-               </div>
-               <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-                   <input type="submit" value="Отправить">
-               </div>
-           </div>        
-        </form>  
+        <header>
+            <div class="logo">
+                DEAMON
+            </div>
+        </header>
+       
+        <div class="main-content">
+            <form method="POST" action="" class="main-content">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5">
+                        <input type="text" name="startDate" placeholder="Введите начальную дату:">   
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-5 col-lg-5">
+                        <input type="text" name="endDate" placeholder="Введите конечную дату:">
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+                        <input type="submit" value="Отправить">
+                    </div>
+                </div> <!---- End .row ----->        
+            </form>  
+        </div><!---- End .main-content ---->
+        
+        <footer>
+            Copyright © 2016 <a href="https://vk.com/pastushenko_danial" target="_blank">Demon.ru</a>
+        </footer>
+        
         <?php
         if(isset($result)){
          echo $result;
